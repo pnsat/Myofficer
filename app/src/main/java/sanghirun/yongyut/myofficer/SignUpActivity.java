c8b0cef89b0c33df9b500e54eee77362b2393867
masterUng <ubc1515@hotmail.com>
2017-01-15 09:39:18 +0700

    Upload Image to server Success

--- app/src/main/java/sanghirun/yongyut/myofficer/SignUpActivity.java
+++ app/src/main/java/sanghirun/yongyut/myofficer/SignUpActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import org.jibble.simpleftp.SimpleFTP;
+
+import java.io.File;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -147,6 +152,27 @@ public class SignUpActivity extends AppCompatActivity {
         //Find name of Image Choose
         nameImageChooseString = pathImageChooseString.substring(pathImageChooseString.lastIndexOf("/"));
         Log.d("14janV1", "nameImage ==> " + nameImageChooseString);
+
+        //upload Image
+        try {
+
+            //Connect FTP protocal
+            StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                    .Builder().permitAll().build();
+            StrictMode.setThreadPolicy(threadPolicy);
+
+            SimpleFTP simpleFTP = new SimpleFTP();
+            simpleFTP.connect("ftp.swiftcodingthai.com", 21,
+                    "14jan@swiftcodingthai.com", "Abc12345");
+
+            simpleFTP.bin();
+            simpleFTP.cwd("YutImage");
+            simpleFTP.stor(new File(pathImageChooseString));
+            simpleFTP.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
 
