7473376b7e89edb0adec1dbf7fed8041a4379df0
masterUng <ubc1515@hotmail.com>
2017-01-15 11:53:18 +0700

    SING IN OK

--- app/src/main/java/sanghirun/yongyut/myofficer/MainActivity.java
+++ app/src/main/java/sanghirun/yongyut/myofficer/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
     private Button signInButton, signUpButton;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
 
 
     @Override
@@ -20,6 +24,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Bind wiget
         signInButton = (Button) findViewById(R.id.button);
         signUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+
 
         //Button Controller
         signInButton.setOnClickListener(MainActivity.this);
@@ -34,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (view.getId()) {
             case R.id.button:  // for singin
+
+                myAuthen();
+
                 break;
             case R.id.button2:  // for singup
                 startActivity(new Intent(MainActivity.this, SignUpActivity.class));
@@ -41,4 +51,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    private void myAuthen() {
+
+        //Get value
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check User and Password
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have String
+            MyAlert myAlert = new MyAlert(MainActivity.this);
+            myAlert.errorDialog("มีช่องว่าง", "กรุณากรอกทุกช่องค่ะ");
+
+        }
+
+    } // myAuthen
 }   // Main Class
